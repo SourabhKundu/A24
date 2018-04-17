25641b0d234c6c07c458016bb343f17308251cb2
SourabhKundu <38473810+SourabhKundu@users.noreply.github.com>
2018-04-18 00:06:28 +0530

    commit 3

--- A24.c
+++ A24.c
@@ -89,3 +89,97 @@ void priority_process()
     }
     
 }
+int rtime,rcount;
+void round_robin_process()
+{
+    printf("\n\nRound robin processs are running\n");
+    
+    printf("\n\nprocess no\t|before burst time\t|process remaining time\n"); 
+
+    int flag=0;
+        int min=1000;
+        for(int i=0;i<n[1];i++)
+        {
+            if(que2[i].ar_time<min)
+            {
+                min=que2[i].ar_time;
+                rcount=i;
+            }
+        }
+    while(total_burst[1]!=0)
+    {
+              
+        printf("\npro%d\t\t %d\t\t",rcount+1,que2[rcount].remain_time);
+         
+
+        if(que2[rcount].remain_time<=time_quantum && que2[rcount].remain_time>0) 
+            { 
+                remain_total_burst[0]+=time_quantum-que2[rcount].remain_time;
+            
+            intime+=que2[rcount].remain_time; 
+            total_burst[1]-=que2[rcount].remain_time;
+                
+            que2[rcount].remain_time=0; 
+            flag=1; 
+            } 
+            else if(que2[rcount].remain_time>0) 
+            { 
+            que2[rcount].remain_time-=time_quantum; 
+            total_burst[1]-=time_quantum;
+            intime+=time_quantum; 
+            } 
+
+             printf("\t%d\n",que2[rcount].remain_time);
+            if(que2[rcount].remain_time==0 && flag==1) 
+            { 
+                printf("process %d terminated\n",rcount+1);
+            flag=0; 
+            } 
+
+            if(rcount==n[1]-1) 
+            {
+                rcount=0;
+                while(que2[rcount].remain_time==0)
+                    {
+                        rcount++;
+
+                    }    
+
+            }else if(que2[rcount+1].ar_time<=intime) 
+                {
+                    rcount++;
+                    
+                    while(que2[rcount].remain_time==0)
+                    rcount++; 
+                }
+                else 
+                rcount=0;
+        
+
+        flag=0;
+        min=1000;
+
+    }
+
+
+}
+
+int main()
+{
+    printf("Enter max elements for queue 1\n");
+    scanf("%d",&n[0]);
+    printf("Enter priority( 0(max) to 3(min)) ,arrival time and burst time\n");
+    input_data(que1,n[0],0);
+    premain=n[0];
+
+    printf("Enter max elements for queue 2\n");
+    scanf("%d",&n[1]);
+    printf("Enter priority( 3(max) to 6(min)) ,arrival time and burst time\n");
+    input_data(que2,n[1],1);
+    rremain=n[1];
+
+    priority_process();
+    round_robin_process();
+    
+    return 0;
+}
